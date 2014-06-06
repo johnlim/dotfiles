c36bc2e44523302168ca0a9f9e429855b9adc0e6
John Lim <johnlim@Johns-MacBook-Pro.local>
2014-06-06 17:49:51 +0800

    Automate installation of dotfiles.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s ~/git/dotfiles/.bash_profile ~/.bash_profile
+source ~/.bash_profile 
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s ~/git/dotfiles/.vimrc ~/.vimrc
+vim +PluginInstall +qall
