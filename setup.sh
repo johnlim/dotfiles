f128c67d5799a1daa9684408ee211ca6ef0fdaea
johnlim <johnlim.dingwei@gmail.com>
2015-09-07 10:38:45 +0800

    Automatically make a copyfor use in IntelliJ.

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ source ~/.bash_profile
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/git/dotfiles/.vimrc ~/.vimrc
 vim +PluginInstall +qall
+
+cp ~/git/dotfiles/.vimrc ~/.ideavimrc
+
